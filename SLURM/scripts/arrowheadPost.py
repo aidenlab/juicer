9e6f941d3cf20c8d6e7e45e3881291c48d88d69c
Kyle Eagen <keagen@stanford.edu>
2016-04-15 19:37:44 -0700

    Minor updates.

--- SLURM/scripts/arrowheadPost.py
+++ SLURM/scripts/arrowheadPost.py
@@ -3,12 +3,14 @@ import sys
 import math
 import numpy as np
 from subprocess import check_output
+import pybedtools
 
-resolutions = [5000, 2000, 1000, 500]
+resolutions = [5000, 2000, 1000]
 
 topDir = os.getcwd()
 groupname = os.path.split(topDir)[1]
 chroms = ['chr2L', 'chr2R', 'chr3L', 'chr3R', 'chrX', 'chr4']
+chromSizes = {'chr2L': 23011544, 'chr2R': 21146708, 'chr3L': 24543557, 'chr3R': 27905053, 'chrX': 22422827, 'chr4': 1351857}
 
 def wc(filename):
     return int(check_output(["wc", "-l", filename]).split()[0])
@@ -52,7 +54,7 @@ for i in range(0, len(sorted)):
 sortedFiltered = np.delete(sorted, np.unique(idx), axis=0)
 
 # Sort by chromosome, TAD start, TAD end	
-idx2 = np.lexsort((sortedFiltered[:,5], sortedFiltered[:,2], sortedFiltered[:,4], sortedFiltered[:,1], sortedFiltered[:,3], sortedFiltered[:,0]))
+idx2 = np.lexsort((sortedFiltered[:,5].astype(int), sortedFiltered[:,2].astype(int), sortedFiltered[:,4].astype(int), sortedFiltered[:,1].astype(int), sortedFiltered[:,3], sortedFiltered[:,0]))
 finalTADs = [sortedFiltered[i] for i in idx2]
 
 # Remove heterochromatic and poorly assembled chromosomes.
@@ -60,9 +62,67 @@ idx3 = []
 for i in range(0, len(finalTADs)):
 	if finalTADs[i][0] not in chroms:
 		idx3.append(i)
-finalTADs2 = np.delete(finalTADs, idx3, axis=0)		
+finalTADs2 = np.delete(finalTADs, idx3, axis=0)	
 
+# Save the TADs in juicebox format.
 print 'Saving '+str(len(finalTADs2))+' TADs, excluding heterochromatic and poorly mapped chromosomes.'
 np.savetxt(topDir+'/juicer_output/{0}_randomizedpositions_inter_30_TADs_Arrowhead.txt'.format(groupname), np.vstack((header, finalTADs2)), delimiter='\t', fmt='%s')
 
-np.savetxt(topDir+'/juicer_output/{0}_randomizedpositions_inter_30_TADs_Arrowhead.bed'.format(groupname), finalTADs2[:,[0,1,2]], delimiter='\t', fmt='%s')
+# Save the TADs in bed format.
+
+names = np.zeros((len(finalTADs2), 3), dtype='|S12')
+for i in range(0, len(finalTADs2)):
+	names[i] = ['TAD_{0}'.format(i), '0', '.']
+
+np.savetxt(topDir+'/juicer_output/{0}_randomizedpositions_inter_30_TADs_Arrowhead.bed'.format(groupname), np.hstack((finalTADs2[:,[0,1,2]], names)), delimiter='\t', fmt='%s')
+
+# Save the boundaries in bed format.
+starts = np.zeros((len(finalTADs2), 3), dtype='|S12')
+for i in range(0, len(starts)):
+	starts[i] = ['TAD_start', '0', '+']
+startBoundary = np.hstack((np.hstack((finalTADs2[:,[0,1,]], finalTADs2[:,[1,]].astype(int)+1)), starts))
+
+stops = np.zeros((len(finalTADs2), 3), dtype='|S12')
+for i in range(0, len(starts)):
+	stops[i] = ['TAD_stop', '0', '-']
+endBoundary = np.hstack((np.hstack((np.hstack((finalTADs2[:,[0,]], finalTADs2[:,[2,]])), finalTADs2[:,[2,]].astype(int)+1)), stops))
+
+np.savetxt(topDir+'/juicer_output/TADs/{0}_allBoundary.bed'.format(groupname), np.vstack((startBoundary, endBoundary)), delimiter='\t', fmt='%s')
+
+# Save the interbands in bed format.  Separate into left and right interbands.
+
+TADsbed = pybedtools.BedTool(topDir+'/juicer_output/{0}_randomizedpositions_inter_30_TADs_Arrowhead.bed'.format(groupname))
+dm3_major_bed = pybedtools.BedTool('/scratch/PI/kornberg/keagen/referencegenomes/fly/dm3/dm3.major.bed')
+TADsbed.complement(g='/scratch/PI/kornberg/keagen/referencegenomes/fly/dm3/dm3.chrom.sizes').intersect(dm3_major_bed, u=True, output=topDir+'/juicer_output/{0}_randomizedpositions_inter_30_interbands_Arrowhead.bed'.format(groupname))
+
+leftInterbands = np.zeros((len(finalTADs2), 3), dtype='|S12')
+for i in range(0, len(finalTADs2)):
+	if finalTADs2[i-1][0] == finalTADs2[i][0]: # Previous TAD on same chromosome
+		leftInterbands[i][0] = finalTADs2[i][0]
+		leftInterbands[i][1] = finalTADs2[i-1][2]
+		leftInterbands[i][2] = finalTADs2[i][1]
+	else: # Previous TAD on different chromosome, interband start is the start of the chromosome
+		leftInterbands[i][0] = finalTADs2[i][0]
+		leftInterbands[i][1] = 0
+		leftInterbands[i][2] = finalTADs2[i][1]
+
+rightInterbands = np.zeros((len(finalTADs2), 3), dtype='|S12')
+for i in range(0, len(finalTADs2)):
+	if i+1 == len(finalTADs2):
+		rightInterbands[i][0] = finalTADs2[i][0]
+		rightInterbands[i][1] = finalTADs2[i][2]
+		rightInterbands[i][2] = chromSizes[finalTADs2[i][0]]
+	elif finalTADs2[i+1][0] == finalTADs2[i][0]: # Next TAD on same chromosome
+		rightInterbands[i][0] = finalTADs2[i][0]
+		rightInterbands[i][1] = finalTADs2[i][2]
+		rightInterbands[i][2] = finalTADs2[i+1][1]
+	else: # Next TAD on different chromosome, interband end is the end of the chromosome
+		rightInterbands[i][0] = finalTADs2[i][0]
+		rightInterbands[i][1] = finalTADs2[i][2]
+		rightInterbands[i][2] = chromSizes[finalTADs2[i][0]]
+
+np.savetxt(topDir+'/juicer_output/TADs/{0}_leftInterbands.bed'.format(groupname), np.hstack((leftInterbands, names)), delimiter='\t', fmt='%s')
+
+np.savetxt(topDir+'/juicer_output/TADs/{0}_rightInterbands.bed'.format(groupname), np.hstack((rightInterbands, names)), delimiter='\t', fmt='%s')
+
+
--- SLURM/scripts/hiccupsPost.py
+++ SLURM/scripts/hiccupsPost.py
@@ -6,8 +6,11 @@ from subprocess import check_output
 
 topDir = os.getcwd()
 groupname = os.path.split(topDir)[1]
-loopsFile = sys.argv[1]
+loopsFile = topDir+'/juicer_output/{0}_randomizedpositions_inter_30_loops_hiccups.txt'.format(groupname)
 chroms = ['chr2L', 'chr2R', 'chr3L', 'chr3R', 'chrX', 'chr4']
+loopsDir = topDir+'/juicer_output/loops'
+if not os.path.exists(loopsDir):
+	os.mkdir(loopsDir)
 
 def wc(filename):
     return int(check_output(["wc", "-l", filename]).split()[0])
@@ -27,6 +30,27 @@ for i in range(0, len(hiccupsIn)):
 hiccupsOut = np.delete(hiccupsIn, idx, axis=0)
 
 print 'Saving '+str(len(hiccupsOut))+' loops.'
-np.savetxt(loopsFile, np.vstack((header, hiccupsOut)), delimiter='\t', fmt='%s')
+np.savetxt(topDir+'/juicer_output/{0}_randomizedpositions_inter_30_loops_hiccups_gutter10.txt'.format(groupname), np.vstack((header, hiccupsOut)), delimiter='\t', fmt='%s')
 
-#np.savetxt(topDir+'/juicer_output/{0}_inter_30_loops_guttered.bed'.format(groupname), hiccupsOut[:,[0,1,5]], delimiter='\t', fmt='%s')
+# Create a bed file of loops.  For loop "regions" use the outermost boundaries of loop anchors.
+np.savetxt(topDir+'/juicer_output/{0}_randomizedpositions_inter_30_loops_hiccups_gutter10.bed'.format(groupname), hiccupsOut[:,[0,1,5]], delimiter='\t', fmt='%s')
+
+# Get loop anchors.  For any anchor less than 5 kb, extend to 5 kb.
+
+anchors1 = np.zeros((len(hiccupsOut), 3), dtype='S10')
+for i in range(0, len(hiccupsOut)):
+	line = hiccupsOut[i]
+	if int(line[2]) - int(line[1]) < 5000:
+		anchors1[i] = [line[0], int(round((float(line[2]) + float(line[1]))/2 - 2500)), int(round((float(line[2]) + float(line[1]))/2 + 2500))]
+	else:
+		anchors1[i] = line[0:3]
+
+anchors2 = np.zeros((len(hiccupsOut), 3), dtype='S10')
+for i in range(0, len(hiccupsOut)):
+	line = hiccupsOut[i]
+	if int(line[5]) - int(line[4]) < 5000:
+		anchors2[i] = [line[0], int(round((float(line[5]) + float(line[4]))/2 - 2500)), int(round((float(line[5]) + float(line[4]))/2 + 2500))]
+	else:
+		anchors2[i] = line[3:6]
+
+np.savetxt(loopsDir+'/{0}_loopAnchors_gutter10.bed'.format(groupname), np.vstack((anchors1, anchors2)), delimiter='\t', fmt='%s')
