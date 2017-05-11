f69ad69a5805b42f50925d30ff99b5ee0ff472f9
nchernia <nchernia@gmail.com>
2017-05-11 15:59:18 +0200

    modified cleanup script; fixed bug in split rmdups

--- SLURM/scripts/cleanup.sh
+++ SLURM/scripts/cleanup.sh
@@ -27,5 +27,21 @@
 # Juicer version 1.5
 total=`ls -l aligned/merged_sort.txt | awk '{print $5}'`
 total2=`ls -l aligned/merged_nodups.txt aligned/dups.txt aligned/opt_dups.txt | awk '{sum = sum + $5}END{print sum}'`
-if [ $total -eq $total2 ]; then rm aligned/merged_sort.txt; rm -r splits; else echo "Problem: The sum of merged_nodups and the dups files is not the same size as merged_sort.txt"; fi
-#bzip2 fastq/*.fastq
+if [ $total -eq $total2 ] 
+then 
+    rm aligned/merged_sort.txt 
+    rm -r splits 
+    for i in fastq/*.fastq
+    do
+        gzip $i
+    done
+    gzip aligned/merged_nodups.txt
+    gzip aligned/dups.txt
+    gzip aligned/opt_dups.txt
+    gzip aligned/abnormal.sam
+    gzip aligned/collisions.txt
+    gzip aligned/unmapped.sam
+else 
+    echo "Problem: The sum of merged_nodups and the dups files is not the same size as merged_sort.txt"
+    echo "Did NOT clean up";
+fi
