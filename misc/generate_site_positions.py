fe3ccafdfe6b2d6a17037eee0d2e06dc4dc9842c
Kornel Ecsedi <44684410+ecsedi@users.noreply.github.com>
2018-11-02 19:21:23 +0100

    Introducing wildcard characters in test strings
    This is a complete rewrite of generate_site_positions.py.
    The new version runs under python3 as well as python2.
    It corrects an error found in the previous version where the very last number in the result was wrong (it was one too many).
    It can work with multiple test strings at the same time.
    Its search algorithm uses regular expressions so wildcard symbols (N, M, R, W, Y, S, K, H, B, V, D) in test strings can also be used to find matches.
    The price of using regular expressions is slower execution.
    The script contains a dictionary for genome/filename mappings and another one for enzyme/test string mappings which users can modify easily according to their needs.

--- misc/generate_site_positions.py
+++ misc/generate_site_positions.py
@@ -14,6 +14,34 @@ def usage():
 
 # ------------------------------------------------------------------------------
 
+def pattern2regexp(pattern):
+
+  wildcards = {
+    'N': '[ACGT]',
+    'M': '[AC]',
+    'R': '[AG]',
+    'W': '[AT]',
+    'Y': '[CT]',
+    'S': '[CG]',
+    'K': '[GT]',
+    'H': '[ACT]',
+    'B': '[CGT]',
+    'V': '[ACG]',
+    'D': '[AGT]',
+  }
+
+  if isinstance(pattern, str):
+    plen    = len(pattern)
+    pattern = pattern.upper()
+    for p, r in wildcards.items():
+      pattern = re.sub(p, r, pattern)
+    # Create a tuple containing the regular expression and the original pattern length.
+    return (re.compile(pattern.lower()), plen)
+
+  return [ pattern2regexp(p) for p in pattern ]
+
+# ------------------------------------------------------------------------------
+
 def process_args(args):
 
   # Genome to filename mappings
@@ -34,13 +62,26 @@ def process_args(args):
   # Multiple patterns are supported in the form of lists. If you enumerate more
   # than one patterns for an enzyme, then a match will be reported when at least
   # one of them can be found at a given position.
+  #
+  # Wildcards:
+  #   N: A, C, G or T
+  #   M: A or C
+  #   R: A or G
+  #   W: A or T
+  #   Y: C or T
+  #   S: C or G
+  #   K: G or T
+  #   H: A, C or T
+  #   B: C, G or T
+  #   V: A, C or G
+  #   D: A, G or T
 
   patterns = {
-    'HindIII'  : 'AAGCTT',
-    'DpnII'    : 'GATC',
-    'MboI'     : 'GATC',
-    'Sau3AI'   : 'GATC',
-    'MultiTest': [ 'AAGCTT', 'GATC' ],
+    'HindIII'     : 'AAGCTT',
+    'DpnII'       : 'GATC',
+    'MboI'        : 'GATC',
+    'Sau3AI'      : 'GATC',
+    'Arima'       : [ 'GATC', 'GANTC' ],
   }
 
   if len(args) != 3 and len(args) != 4:
@@ -61,10 +102,7 @@ def process_args(args):
     usage()
 
   if enzyme in patterns:
-    if isinstance(patterns[enzyme], str):
-      pattern = patterns[enzyme].lower()
-    else:
-      pattern = [ p.lower() for p in set(patterns[enzyme]) ]
+    pattern = pattern2regexp(patterns[enzyme])
   else:
     print('<restriction enzyme> must be one of {}'.format(list(patterns.keys())), file=sys.stderr)
     usage()
@@ -86,8 +124,8 @@ def process_single_pattern(params):
   f = open(params['inputfile' ], 'r')
   g = open(params['outputfile'], 'w')
 
-  pattern    = params['pattern']
-  patternlen = len(pattern)
+  pattern    = params['pattern'][0] # A regular expression.
+  patternlen = params['pattern'][1] # An integer number.
 
   segment = ''
   counter = 0
@@ -123,7 +161,8 @@ def process_single_pattern(params):
 
       while len(segment) > patternlen:
         segment = segment[1:]
-      if segment == pattern:
+
+      if re.match(pattern, segment):
         g.write(' ' + str(counter - patternlen + 1)) # There is a match here.
 
   # Close the last sequence.
@@ -145,15 +184,15 @@ def process_multi_pattern(params):
   f = open(params['inputfile' ], 'r')
   g = open(params['outputfile'], 'w')
 
-  patterns = params['pattern']
-  maxsize  = 0                 # Maximum segment size (size of the longest pattern).
-  minsize  = len(patterns[0])  # Minimum segment size (size of the shortest pattern).
+  patterns = [ p[0] for p in params['pattern'] ]
+  maxsize  = 0                       # Maximum segment size (size of the longest pattern).
+  minsize  = params['pattern'][0][1] # Minimum segment size (size of the shortest pattern).
 
-  for p in patterns:
-    if len(p) > maxsize:
-      maxsize = len(p)
-    if len(p) < minsize:
-      minsize = len(p)
+  for p in params['pattern']:
+    if p[1] > maxsize:
+      maxsize = p[1]
+    if p[1] < minsize:
+      minsize = p[1]
 
   segment = ''
   counter = 0
@@ -170,13 +209,10 @@ def process_multi_pattern(params):
 
       while len(segment) > minsize:
         segment = segment[1:]
-        match   = False
         for p in patterns:
-          if segment.startswith(p):
-            match = True
+          if re.match(p, segment):
+            g.write(' ' + str(counter - len(segment) + 1))
             break
-        if match:
-          g.write(' ' + str(counter - len(segment) + 1))
 
       if counter > 0:
         g.write(' ' + str(counter)) # Close the previous sequence here.
@@ -204,25 +240,19 @@ def process_multi_pattern(params):
       # Do pattern matching only if segment size equals maxsize.
 
       if len(segment) == maxsize:
-        match = False
         for p in patterns:
-          if segment.startswith(p):
-            match = True
+          if re.match(p, segment):
+            g.write(' ' + str(counter - maxsize + 1)) # maxsize == len(segment)
             break
-        if match:
-          g.write(' ' + str(counter - maxsize + 1)) # maxsize == len(segment)
 
   # Finish the last sequence.
 
   while len(segment) > minsize:
     segment = segment[1:]
-    match   = False
     for p in patterns:
-      if segment.startswith(p):
-        match = True
+      if re.match(p, segment):
+        g.write(' ' + str(counter - len(segment) + 1))
         break
-    if match:
-      g.write(' ' + str(counter - len(segment) + 1))
 
   if counter > 0:
     g.write(' ' + str(counter))
@@ -238,7 +268,7 @@ def process_multi_pattern(params):
 
 params = process_args(sys.argv)
 
-if isinstance(params['pattern'], str):
-  process_single_pattern(params)
-else:
+if isinstance(params['pattern'], list):
   process_multi_pattern(params)
+else:
+  process_single_pattern(params)
