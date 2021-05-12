003efad4dbb6fdc6576e84204ed80a97410780b1
Neva C. Durand <nchernia@gmail.com>
2021-05-12 14:14:11 -0500

    typo

--- SLURM/scripts/sam_to_mnd.sh
+++ SLURM/scripts/sam_to_mnd.sh
@@ -1,3 +1,3 @@
 # How to create old style merged_nodups.txt for 3D-DNA
 # File should be dedupped SAM
-saamtools view -O SAM -F 1024 $1 | awk -v mnd=1 -f sam_to_pre.awk 
+samtools view -O SAM -F 1024 $1 | awk -v mnd=1 -f sam_to_pre.awk 
